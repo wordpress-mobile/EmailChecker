86bddb0989ee3d371fe38d1d2e60a4b5222574c7
Maxime Biais <maxime.biais@gmail.com>
2013-11-13 11:16:27 +0100

    android library

---
+++ android/build.sh
@@ -0,0 +1,5 @@
+ndk-build clean
+rm -rf libs
+ndk-build
+gradle build
+mvn install:install-file -Dfile=build/libs/EmailChecker-0.1-debug.aar -DgroupId=org.wordpress -DartifactId=emailchecker -Dpackaging=aar -Dversion=0.1

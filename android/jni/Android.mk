ebf7c75065452b782d38a69f86340ae32418e830
Maxime Biais <maxime.biais@gmail.com>
2013-11-13 11:29:21 +0100

    fix android native build

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -6,14 +6,13 @@ LOCAL_CFLAGS := -std=gnu++11 -fno-exceptions -fno-rtti
 
 #LOCAL_CPP_FEATURES += rtti
 
-EMAILCHECKER_SOURCES = $(shell find $(LOCAL_PATH)/../../../common/ -name "*.cpp"|sed 's+$(LOCAL_PATH)/++')
+EMAILCHECKER_SOURCES = $(shell find $(LOCAL_PATH)/../../common/ -name "*.cpp"|sed 's+$(LOCAL_PATH)/++')
 
 ANDROID_SOURCES = $(shell find $(LOCAL_PATH)/../jni -name "*.cpp"|sed 's+$(LOCAL_PATH)/++')
 $($warning "HEY: " $(ANDROID_SOURCES))
 
 LOCAL_SRC_FILES := $(EMAILCHECKER_SOURCES) $(ANDROID_SOURCES)
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/ $(LOCAL_PATH)/../../../common/ $(LOCAL_PATH)/android-ndk
-#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/wpmobile $(LOCAL_PATH)/android-ndk/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ $(LOCAL_PATH)/../../common/
 
 include $(BUILD_SHARED_LIBRARY)
 

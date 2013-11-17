efaf2d7a77f72850ee70c65576a831c731b44430
Maxime Biais <maxime.biais@gmail.com>
2013-11-17 12:16:08 +0100

    comment build.sh file

--- android/build.sh
+++ android/build.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
 ndk-build clean
 rm -rf libs
 gradle build
+
+# Push to the local maven repo
 mvn install:install-file -Dfile=build/libs/EmailChecker-0.1-debug.aar -DgroupId=org.wordpress -DartifactId=emailchecker -Dpackaging=aar -Dversion=0.1

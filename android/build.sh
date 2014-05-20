ccb46c05e06bd57152f1107d40148485db0c5521
Maxime Biais <maxime.biais@gmail.com>
2014-05-20 09:27:30 +0800

    update 0.9 android-gradle-plugin

--- android/build.sh
+++ android/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-./gradlew build
+./gradlew clean
+./gradlew assemble
 
-VERSION=0.2
+VERSION=0.3
 GROUP_ID=org.wordpress
 
 # Push to the local maven repo
-mvn install:install-file -Dfile=build/libs/EmailChecker-$VERSION.aar -DgroupId=$GROUP_ID -DartifactId=emailchecker -Dpackaging=aar -Dversion=$VERSION
+mvn install:install-file -Dfile=build/libs/EmailChecker-$VERSION-release.aar -DgroupId=$GROUP_ID -DartifactId=emailchecker -Dpackaging=aar -Dversion=$VERSION
 
 mvn install:install-file -Dfile=build/libs/EmailChecker-$VERSION-debug.aar -DgroupId=$GROUP_ID -DartifactId=emailchecker-debug -Dpackaging=aar -Dversion=$VERSION

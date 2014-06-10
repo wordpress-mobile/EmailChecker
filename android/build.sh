e27abf33bd0e1a8367b529b3d48fc9171ef990d9
Maxime Biais <maxime.biais@gmail.com>
2014-06-10 15:26:35 +0800

    rename android project EmailChecker -> email-checker

--- android/build.sh
+++ android/build.sh
@@ -7,6 +7,10 @@ VERSION=0.3
 GROUP_ID=org.wordpress
 
 # Push to the local maven repo
-mvn install:install-file -Dfile=build/libs/EmailChecker-$VERSION-release.aar -DgroupId=$GROUP_ID -DartifactId=emailchecker -Dpackaging=aar -Dversion=$VERSION
+mvn install:install-file -Dfile=build/libs/email-checker-$VERSION-release.aar \
+ -DgroupId=$GROUP_ID -DartifactId=emailchecker -Dpackaging=aar \
+ -Dversion=$VERSION
 
-mvn install:install-file -Dfile=build/libs/EmailChecker-$VERSION-debug.aar -DgroupId=$GROUP_ID -DartifactId=emailchecker-debug -Dpackaging=aar -Dversion=$VERSION
+mvn install:install-file -Dfile=build/libs/email-checker-$VERSION-debug.aar \
+ -DgroupId=$GROUP_ID -DartifactId=emailchecker-debug -Dpackaging=aar \
+ -Dversion=$VERSION
